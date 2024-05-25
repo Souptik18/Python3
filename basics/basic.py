dd738a03f1f9e3bbd025ed5457aeda6ea602215b
Souptik18 <souptiksarkar2017@gmail.com>
2024-05-25 13:29:31 +0530

    upd

--- basics/basic.py
+++ basics/basic.py
@@ -1,13 +1,14 @@
 
 # print("Hello World")
-def add(a,b):
-  return a+b
+# def add(a,b):
+#   return a+b
 
-print (add(2,4))
+# print (add(2,4))
 
-arr = [1,2,3,4,5,6,7,8,9,10]
-arr[0]= 10
-print(arr)
+# arr = [1,2,3,4,5,6,7,8,9,10]
+# arr[0]= 10
+# print(arr)
 
 # import from importlib import reload
+
 
