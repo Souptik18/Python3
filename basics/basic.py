d74fe0319b1baaf1b2206f63a83fd0b9cce01c99
Souptik18 <souptiksarkar2017@gmail.com>
2024-06-09 21:21:40 +0530

    code

--- basics/basic.py
+++ basics/basic.py
@@ -1,24 +1,24 @@
 
-# print("Hello World")
-# def add(a,b):
-#   return a+b
+print("Hello World")
+def add(a,b):
+  return a+b
 
-# print (add(2,4))
+print (add(2,4))
 
-# arr = [1,2,3,4,5,6,7,8,9,10]
-# arr[0]= 10
-# print(arr)
+arr = [1,2,3,4,5,6,7,8,9,10]
+arr[0]= 10
+print(arr)
 
-# import from importlib import reload
-# def add(a,b):
-#   print(a+b)
+import from importlib import reload
+def add(a,b):
+  print(a+b)
 
-# def divide(a,b):
-#   print(a/b)
+def divide(a,b):
+  print(a/b)
 
-# divide(2,4)
+divide(2,4)
 
-# import pandas
+import pandas
 
-# print("Hello")
+print("Hello")
 
